140e1716603820f4253eab29d492de288d66c730
Berrag <berragrizlene1977@gmail.com>
2026-03-10 15:13:17 +0100

    liste

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,37 +1,98 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  ActivityIndicator,
+  TouchableOpacity,
+} from "react-native";
+import { getAnimes } from "../services/api.service";
+import { Anime } from "../types/types";
 import { useNavigation } from "@react-navigation/native";
-import { RootStackParamList } from "../navigation/AppNavigator";
-
-type HomeScreenNavigationProp = NativeStackNavigationProp<
-  RootStackParamList,
-  "Home"
->;
 
 export default function HomeScreen() {
-  const navigation = useNavigation<HomeScreenNavigationProp>();
+  const [animes, setAnimes] = useState<Anime[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const navigation = useNavigation<any>();
+
+  useEffect(() => {
+    const fetchAnimes = async () => {
+      try {
+        const response = await getAnimes(1);
+        setAnimes(response.data);
+      } catch (error) {
+        console.error("Erreur API :", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAnimes();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  const renderAnime = ({ item }: { item: Anime }) => (
+    <TouchableOpacity
+      style={styles.card}
+      onPress={() => navigation.navigate("Detail", { id: item.mal_id })}
+    >
+      <Image source={{ uri: item.images.jpg.image_url }} style={styles.image} />
+
+      <View style={styles.info}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.score}>⭐ {item.score}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Home Screen</Text>
-      <Button
-        title="Voir le détail"
-        onPress={() => navigation.navigate("Detail", { id: 1 })}
-      />
-    </View>
+    <FlatList
+      data={animes}
+      keyExtractor={(item) => item.mal_id.toString()}
+      renderItem={renderAnime}
+      contentContainerStyle={styles.list}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  list: {
+    padding: 16,
+  },
+  card: {
+    marginBottom: 20,
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    overflow: "hidden",
+    elevation: 3,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  info: {
+    padding: 12,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  score: {
+    marginTop: 5,
+  },
+  center: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-  title: {
-    fontSize: 22,
-    fontWeight: "bold",
-    marginBottom: 20,
   },
 });
